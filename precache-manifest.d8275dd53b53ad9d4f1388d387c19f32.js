f34186bf9930a0b3c4a2d7752e2f9133c6d6a1ad
killeraliens <killeraliens@outlook.com>
2019-10-25 18:09:21 -0700

    Updates

---
+++ precache-manifest.d8275dd53b53ad9d4f1388d387c19f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084a86eaee0223eacdde88f0fcd6e1aa",
+    "url": "/alexandra-brinn-cv/index.html"
+  },
+  {
+    "revision": "eb5dcd64d58f9fce597c",
+    "url": "/alexandra-brinn-cv/static/css/main.fa6048ae.chunk.css"
+  },
+  {
+    "revision": "3f06b528500cff6e4b89",
+    "url": "/alexandra-brinn-cv/static/js/2.18bdca02.chunk.js"
+  },
+  {
+    "revision": "eb5dcd64d58f9fce597c",
+    "url": "/alexandra-brinn-cv/static/js/main.a87336cf.chunk.js"
+  },
+  {
+    "revision": "7e45257a865d9b23db71",
+    "url": "/alexandra-brinn-cv/static/js/runtime-main.cb85bc1a.js"
+  },
+  {
+    "revision": "8e731084d562eef1af9082620542d585",
+    "url": "/alexandra-brinn-cv/static/media/alexandra-brinn-campbell-cv.8e731084.pdf"
+  },
+  {
+    "revision": "ae38706a9e4839108387365ccb95893a",
+    "url": "/alexandra-brinn-cv/static/media/alexandra-brinn-profile.ae38706a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.5dfe93c5bd67f814e60cdfd059c9bad6.js"
+  "/alexandra-brinn-cv/precache-manifest.d8275dd53b53ad9d4f1388d387c19f32.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alexandra-brinn-cv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
