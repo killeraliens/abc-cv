f252e1faacee510d15bc49eeaaad0b0e4b97c1dd
killeraliens <killeraliens@outlook.com>
2020-04-08 12:06:19 -0700

    useeffect exhaustive depths eslint free

--- src/components/PdfDownload.js
+++ src/components/PdfDownload.js
@@ -5,24 +5,24 @@ import { faFileDownload } from '@fortawesome/free-solid-svg-icons'
 export default function PdfDownload ({ path }) {
   const [fileSize, setFileSize] = useState('')
 
-  const getSize = (href) => {
-    let http = new XMLHttpRequest()
-
-    http.open('HEAD', path, true)
-    http.onreadystatechange = function () {
-      if (this.readyState === this.DONE) {
-        if (this.status === 200) {
-          const newSize = this.getResponseHeader('content-length')
-          return setFileSize(newSize)
-        }
-      }
-    }
-    http.send()
-  }
 
   useEffect(() => {
+    const getSize = (href) => {
+      let http = new XMLHttpRequest()
+
+      http.open('HEAD', path, true)
+      http.onreadystatechange = function () {
+        if (this.readyState === this.DONE) {
+          if (this.status === 200) {
+            const newSize = this.getResponseHeader('content-length')
+            return setFileSize(newSize)
+          }
+        }
+      }
+      http.send()
+    }
     return getSize(path)
-  }, [])
+  }, [path])
 
   return (
     <a id="pdfLink" className="live-only" href={path} rel="noopener noreferrer" target="_blank" download>
